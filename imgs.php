2903123cac797e9bbd5895c07fe3b67fcb2c5c85
lavankumar <chlavankumar179@gmail.com>
2020-09-18 19:17:43 +0530

    fourth commit

--- imgs.php
+++ imgs.php
@@ -1,8 +1,10 @@
 <?php
     $files = glob("uploads/*.*");
+    $name = "";
      for ($i=0; $i<count($files); $i++)
       {
         $image = $files[$i];
+        $name = basename($image);
         $supported_file = array(
                 'gif',
                 'jpg',
@@ -12,7 +14,7 @@
 
          $ext = strtolower(pathinfo($image, PATHINFO_EXTENSION));
          if (in_array($ext, $supported_file)) { // show only image name if you want to show full path then use this code // echo $image."<br />";
-             echo '<div class="gallery-container"><div class="gallery-item"><div class="image"><img src="'.$image .'" alt="Random image"/></div><div class="text">"'.basename($image).'"</div></div></div>"';
+             echo '<div class="gallery-container"><div class="gallery-item"><div class="image"><img src="'.$image .'" alt="Randomimage"/></div><div class="text">'.$name .'</div></div></div>';
             } else {
                 continue;
             }
