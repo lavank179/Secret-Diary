6ebec09cbebaca19314d029bf83303f2abde789c
lavank179 <chlavankumar179@gmail.com>
2021-05-24 12:30:26 +0530

    Redesigning

--- connection.php
+++ connection.php
@@ -1,11 +1,10 @@
 <?php
 
-    $link = mysqli_connect('127.0.0.1', 'root', 'lk2356', 'lavan1');
+    $link = mysqli_connect('localhost', 'root', '', 'diary');
         
         if (mysqli_connect_error()) {
             
             die ("Database Connection Error");
             
         }
-
 ?>
